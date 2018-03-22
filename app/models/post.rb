e54b86f6fbac3f24fa18eea61b6fdcff84e3c530
Rachel Miller <raeanne1991@gmail.com>
2018-03-22 14:37:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :tags
+  has_many :tags, through: :post_tag 
   validates :name, presence: true
   validates :content, presence: true
 end
